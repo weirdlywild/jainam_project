298b46f9aec66ae565133cb3b06f8bf47bafb9ce
Raj Solanki <solankiraj10897@google.com>
2020-05-03 20:08:49 +0530

    Excel file load | step2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var buttonclickgender = null;
 var buttonclicksmoker = null;
 var valuebtn = null;
+var url = "https://weirdlywild.github.io/jainam_project/teacherslife-rates.xlsx";
+var yearselect = "10year";
 $(document).ready(function () {
     $("#malebtn").click(function () {
         buttonclickgender = "male";
@@ -40,11 +42,49 @@ $(document).ready(function () {
         }
     })
 
+    document.getElementById("btnresult").innerHTML = "10 years";
+    $(".dummyyear").click(function () {
+        if (yearselect === '10year') {
+            document.getElementById("btnresult").innerHTML = "10 years";
+            $("#10years").removeClass("activebtn");
+            $("#20years").removeClass("activebtn");
+            $("#till65").removeClass("activebtn");
+            $("#10years").addClass("activebtn");
+
+        }
+        else if (yearselect === '20year') {
+            document.getElementById("btnresult").innerHTML = "20 years";
+            $("#10years").removeClass("activebtn");
+            $("#20years").removeClass("activebtn");
+            $("#till65").removeClass("activebtn");
+            $("#20years").addClass("activebtn");
+        }
+        else if (yearselect === '65year') {
+            document.getElementById("btnresult").innerHTML = "to age 65";
+            $("#10years").removeClass("activebtn");
+            $("#20years").removeClass("activebtn");
+            $("#till65").removeClass("activebtn");
+            $("#till65").addClass("activebtn");
+        }
+    })
+
 });
 $(function () {
     $('[data-toggle="popover"]').popover()
 })
 
 
+// Sheet
+var req = new XMLHttpRequest();
+req.open("GET", url, true);
+req.responseType = "arraybuffer";
 
+req.onload = function (e) {
+    var data = new Uint8Array(req.response);
+    var workbook = XLSX.read(data, { type: "array" });
+    console.log(workbook);
 
+    /* DO SOMETHING WITH workbook HERE */
+}
+
+req.send();
